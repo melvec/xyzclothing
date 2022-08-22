f8b6f8f5f843bb2ccd4bbac51ab1dad3d0dbd37d
Melissa Vela <melissa@instarent.com.au>
2022-08-23 00:16:04 +1000

    first table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
+import data from "./products.json";
+
 
 function App() {
+  const [products, setProducts] = useState(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<React.Fragment>
+
+<div className="App">
+     <table>
+       <thead>
+         <tr>
+           <th>Id</th>
+           <th>Name</th>
+           <th>Description</th>
+           <th>Price</th>
+         </tr>
+       </thead>
+       <tbody>
+         {products.map ( (product) => (
+          <tr>
+           <td>{product.id}</td>
+           <td>{product.name}</td>
+           <td>{product.description}</td>
+           <td>{product.price.amount} {product.price.base}</td>
+         </tr>
+         ))}
+       </tbody>
+     </table>
     </div>
+</React.Fragment>
+
+  
   );
 }
 
